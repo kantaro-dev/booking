78359dda5c6850fa9d6a44799dd22618d7cddcff
kantaro <kantarodev@gmail.com>
2022-05-28 12:17:13 +0900

    Sharing data with templates

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -2,7 +2,9 @@ package handlers
 
 import (
 	"bookings/pkg/config"
+	"bookings/pkg/models"
 	"bookings/pkg/render"
+
 	"net/http"
 )
 
@@ -28,11 +30,18 @@ func NewHandlers(r *Repository) {
 
 // Home is the home page handler
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "home.page.tmpl")
+	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
 
 }
 
 // About is the about page handler
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "about.page.tmpl")
+	// perform some logic
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello, again."
+
+	// send the data to the template
+	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
+		StringMap: stringMap,
+	})
 }
